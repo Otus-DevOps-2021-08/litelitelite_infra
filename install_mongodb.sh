80ef9162777f1d42a68ad2648ddce27e025e9e44
litelitelite <tolstenkov_ii@magnit.ru>
2021-09-19 11:13:37 +0000

    install_mongo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update && sudo apt upgrade && sudo apt install -y mongodb-org
 sudo systemctl start mongod && sudo systemctl enable mongod
